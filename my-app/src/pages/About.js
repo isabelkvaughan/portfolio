3fdda23783657750f647e8e95b15f82877ecd8d1
Isabel <isabelkvaughan@isabels-mbp.home>
2023-07-09 17:06:10 +0200

    Images added to About

--- my-app/src/pages/About.js
+++ my-app/src/pages/About.js
@@ -5,7 +5,7 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
-    height: '100vh', // Adjust the height based on your needs
+    height: '100vh',
   },
   heading: {
     textAlign: 'center',
@@ -17,7 +17,28 @@ const styles = {
   body: {
     textAlign: 'center',
     width: '50%'
-  }
+  },
+  imageContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    width: '100%',
+    margin: '10px 0',
+  },
+  image: {
+    width: 'calc(33.33% - 10px)',
+    height: '300px',
+    backgroundColor: '#A6C2EE',
+  },
+  '@media (max-width: 767px)': {
+    imageContainer: {
+      flexDirection: 'column',
+      margin: '10px 0', // Mobile: Adjust the margin based on your needs
+    },
+    image: {
+      width: '100%', // Mobile: Full width, one image per row
+      height: '300px', // Mobile: Adjust the height based on your needs
+    },
+  },
 };
 
 export default function About() {
@@ -32,8 +53,15 @@ export default function About() {
         imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
         in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
       </p>
-      
+      <div style={styles.imageContainer}>
+        <div style={styles.image}><img src="./public/jade-stephens-fTVigwKgRYA-unsplash.jpg" /></div>
+        <div style={styles.image}></div>
+        <div style={styles.image}></div>
+      </div>
     </div>
-
   );
 }
+
+{/* <img src="./public/jade-stephens-fTVigwKgRYA-unsplash.jpg" />
+<img src="../../public/about-isabel.jpg" />
+<img src="../../public/about-farm.jpg" /> */}
