9fc79badf9335deb6c636b882e6488d655add167
Isabel <isabelkvaughan@isabels-mbp.home>
2023-07-10 12:06:49 +0200

    Project CSS

--- my-app/src/pages/Portfolio.js
+++ my-app/src/pages/Portfolio.js
@@ -1,36 +1,23 @@
 import React from 'react';
+import '../styles/portfolio.css';
 import Project from '../components/Project';
-
-const styles = {
-  projectContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    width: '100%',
-    height: '100vh',
-    backgroundColor: '#F5EFEC'
-  },
-  project: {
-    width: 'calc(33.33% - 10px)',
-    height: '300px',
-  },
-};
 
 export default function Portfolio() {
   return (
-      <div style={styles.projectContainer}>
-        <div style={styles.project}>
+      <div>
+        <div>
           <Project 
           projectTitle= 'DevLearn Hub'
           liveApp= 'https://dev-learn-app.herokuapp.com/'
           repoName= 'dev-learn-hub'/>
         </div>
-        <div style={styles.project}>
+        <div>
           <Project 
           projectTitle= 'DevLearn Hub'
           liveApp= 'https://dev-learn-app.herokuapp.com/'
           repoName= 'dev-learn-hub'/>
         </div>
-        <div style={styles.project}>
+        <div>
           <Project 
           projectTitle= 'DevLearn Hub'
           liveApp= 'https://dev-learn-app.herokuapp.com/'
