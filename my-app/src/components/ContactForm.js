12fd197a5411765bfa4b0eb482dec5d50589cf0f
Isabel <isabelkvaughan@isabels-mbp.home>
2023-07-11 16:58:01 +0200

    Form styling updates

--- my-app/src/components/ContactForm.js
+++ my-app/src/components/ContactForm.js
@@ -1,28 +1,41 @@
 import React from "react";
 import { Form, Button } from "react-bootstrap";
 
+const styles = {
+  form: {
+    textAlign: 'center',
+    color: '#54667D',
+    paddingTop: '15px'
+  },
+  formBtn: {
+    backgroundColor: '#54667D',
+    border: 'none',
+  }
+};
+
 function ContactForm() {
   return (
+    <div style={styles.form} className='col-8 col-lg-5'>
     <Form>
       <Form.Group controlId="formPlaintextName">
         <Form.Label>Name</Form.Label>
-        <Form.Control type="text" placeholder="Name" />
+        <Form.Control type="text" />
       </Form.Group>
 
-      <Form.Group controlId="formPlaintextEmail">
+      <Form.Group controlId="formPlaintextEmail" className='mt-4'>
         <Form.Label>Email</Form.Label>
-        <Form.Control type="email" placeholder="Email" />
+        <Form.Control type="email" />
       </Form.Group>
 
-      <Form.Group controlId="formPlaintextMessage">
+      <Form.Group controlId="formPlaintextMessage" className='mt-4'>
         <Form.Label>Message</Form.Label>
-        <Form.Control as="textarea" rows={3} placeholder="Enter your message" />
+        <Form.Control as="textarea" rows={3} />
       </Form.Group>
 
-      <Button type="submit">Submit</Button>
+      <Button style={styles.formBtn} type="submit" className='mt-4'>Submit</Button>
     </Form>
+    </div>
   );
 }
 
 export default ContactForm;
-  
--- my-app/src/pages/Contact.js
+++ my-app/src/pages/Contact.js
@@ -2,12 +2,21 @@ import React from 'react';
 import PageHeading from '../components/PageHeading';
 import ContactForm from '../components/ContactForm';
 
+const styles = {
+  formContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+};
+
 
 export default function Contact() {
   return (
     <div>
       <PageHeading pageHeading= 'Contact Isabel'/>
+      <div style={styles.formContainer}>
       <ContactForm />
+      </div>
     </div>
     
   );
