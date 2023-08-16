a4fb013943a27bfb59da76248b9f213e3318858d
Isabel <isabelkvaughan@isabels-mbp.home>
2023-08-16 17:44:48 +0200

    Resume updates

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import PageHeading from '../components/PageHeading';
 import '../styles/resume.css';
-import CV from '../assets/IsabelNoonan_CV.pdf';
 
 export default function Resume() {
   return (
@@ -9,6 +8,10 @@ export default function Resume() {
       <PageHeading pageHeading='Resume' />
       <div className='sectionContainer'>
         <div className='section'>
+          <h4>Technical Skills</h4>
+
+          <div className='skillsContainer'>
+            <div>
           <h5>Front-End Proficiencies</h5>
           <ul>
             <li>HTML</li>
@@ -20,9 +23,9 @@ export default function Resume() {
             <li>Flexbox</li>
             <li>TypeScript</li>
           </ul>
-        </div>
-
-        <div className='section'>
+          </div>
+          <br></br>
+          <div>
           <h5>Back-End Proficiencies</h5>
           <ul>
             <li>APIs</li>
@@ -34,11 +37,35 @@ export default function Resume() {
             <li>REST</li>
             <li>GraphQL</li>
           </ul>
+          </div>
+          </div>
         </div>
-      </div>
 
-      <div>
-      <a href={CV} className='downloadCV' download="IsabelNoonan_CV.pdf"> Download Isabel's CV here</a>
+        <div className='section'>
+          <h4>Professional Skills</h4>
+          <ul>
+            <li><b>Salesforce CRM - </b>Marketing Cloud, Exact Target</li>
+            <li><b>Adobe Suite - </b>InDesign, Illustrator, Photoshop, Premiere, After Effects, Acrobat</li>
+            <li><b>Content Management - </b>Contentful CMS, Wordpress, Squarespace</li>
+            <li><b>Task Management - </b>Monday, Asana, Trello, Jira</li>
+            <li><b>Office Suite and Google Suite, Google Analytics</b></li>
+          </ul>
+        </div>
+
+        <div className='section'>
+        <h4>Education</h4>
+          <p><b>Full Stack Flex Web Development Certificate</b>
+            <br></br>
+            Monash University Boot Camps | 2023</p>
+
+          <p><b>Bachelor of Design (Communication)</b>
+            <br></br>
+            Swinburne University of Technology | 2015 - 2018</p>
+        </div>
+
+        <div className='section'>
+          <h4>Full CV Available Upon Request</h4>
+        </div>
       </div>
     </div>
   );
